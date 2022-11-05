d329f13e3092f2f470830d3013146517c8cada2d
James Hager <jameshager@northwesternmutual.com>
2022-11-05 07:31:10 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,18 +7,31 @@ let package = Package(
     name: "ViewInspector",
     defaultLocalization: "en",
     platforms: [
-        .macOS(.v10_15), .iOS(.v11), .tvOS(.v13), .watchOS(.v7)
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v7)
     ],
     products: [
         .library(
             name: "ViewInspector", targets: ["ViewInspector"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.41.0"),
+    ],
     targets: [
         .target(
-            name: "ViewInspector", dependencies: []),
+            name: "ViewInspector",
+            dependencies: [
+                .product(
+                    name: "ComposableArchitecture",
+                    package: "swift-composable-architecture"),
+            ]),
         .testTarget(
             name: "ViewInspectorTests",
-            dependencies: ["ViewInspector"],
+            dependencies: [
+                "ViewInspector",
+                .product(
+                    name: "ComposableArchitecture",
+                    package: "swift-composable-architecture"),
+            ],
             resources: [.process("TestResources")]),
     ]
 )
